9ae9255b0f5c6a3b07d2be08f23929e1587d3746
Carlos Vinícius <107522005+carlos-vini@users.noreply.github.com>
2023-04-10 12:19:46 -0300

    Add files via upload

---
+++ atividades.js
@@ -0,0 +1,54 @@
+/*
+Escreva um script no JavaScript para unir todos os elementos do array
+*/
+
+// const pokemons = ["Bulbasaur", "Charmander", "Squirtle", "Pidgey", "Ratata"]
+
+// let join = pokemons.join(", ")
+
+// console.log(join)
+
+
+
+/*
+Escreva um script que contenha uma lista de usuários, ordenada por ordem alfabética
+*/
+
+// const users = ["Ampharos", "Heracross", "Kingler", "Altaria"]
+
+// users.sort()
+
+// console.log(users)
+
+
+
+/*
+Escreva uma função que recebe duas strings, digamos a e b, como argumentos.
+
+Se a contém b, anexe b ao início de a, se não, anexe b ao final e retorne a concatenação.
+*/
+
+// let lista = []
+
+// function ab (a, b){
+//     if(a.indexOf(b) !== -1){
+//         lista.push(b + a);
+//         return lista;
+//     } else{
+//         lista.push( a + b)
+//         return lista
+//     }
+// }
+
+// console.log(ab("tips", "a"));
+
+
+
+/*
+Escreva um script que remova o primeiro elemento de um array.
+*/
+
+const pokemons = ["Oddish", "Pikachu", "Blastoise", "Mew", "Skuntank"];
+pokemons.pop();
+
+console.log(pokemons)
---
+++ exemplos.js
@@ -0,0 +1,55 @@
+// const users = ["Ana", "Fernando", "Rafael", "Clara", "Maria"];
+
+// users.push("Roger");
+
+// console.log(users);
+
+
+
+// const person = ["Felipe", 30, "São Paulo", "Masculino", "22233366689"]
+
+// person.pop();
+
+// console.log(person);
+
+
+
+// const fruits = ["Apple", "Orange", "Banana", "Pear"];
+
+// fruits.shift();
+
+// console.log(fruits);
+
+
+
+// const jobs = ["FrontEnd", "BackEnd", "DevOps", "FullStack"];
+
+// jobs.unshift("Design");
+
+// console.log(jobs);
+
+
+
+// const jobs = ["FrontEnd", "BackEnd", "DevOps", "FullStack"];
+// let indexOf = jobs.indexOf("FrontEnd");
+
+// console.log(indexOf);
+
+// const fruits = ["Apple", "Orange", "Banana", "Pear"];
+// let indexOfSearch = fruits.indexOf("Orange", 1);
+
+// console.log(indexOfSearch)
+
+
+
+// const jobs = ["FrontEnd", "BackEnd", "DevOps", "FullStack"];
+
+// jobs.sort();
+
+// console.log(jobs);
+
+const colors = ["Red", "Green", "White", "Black"];
+
+let join = colors.join(" | ")
+
+console.log(join)
